352eb69df5ef1ecdd091adf298a0a450693b8ef4
Thiago Levino Nunes <Thiago.levino@outlook.com>
2023-06-21 22:31:13 -0300

    Introdução a manipulação de strings python

---
+++ Strings.py
@@ -0,0 +1,16 @@
+# Exemplo de programa que aborta o tipo de dado string em Python 
+
+frase = "Óla, Mundo!"
+#Declaração de uma string 
+print("String original: ");
+print(frase);
+print();
+
+#Acessando caracteres individuais
+print("Acessando caracteres individuais: ");
+print("Primeiro caractere: ", frase[0]);
+print("Ultimo caractere: ", frase[-1]);
+
+
+
+

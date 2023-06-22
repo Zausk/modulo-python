5ecfdee40fd469e4b3d24cf57b5959cf50711242
Thiago Levino Nunes <Thiago.levino@outlook.com>
2023-06-21 22:45:49 -0300

    Introdução a manipulação de strings com python

---
+++ numeros.py
@@ -0,0 +1,20 @@
+#Exemplos de programa que aborda o tipo de dado númerico em Python 
+
+#Operações matemáticas básicas
+num1 = 10;
+num2 = 5;
+
+soma              = num1 + num2;
+substracao        = num1 - num2;
+multiplicacao     = num1 * num2;
+divisao           = num1 / num2;
+resto             = num1 % num2;
+potencia          = num1 ** num2;
+
+
+print("soma: ", soma);
+print("substracao: ", substracao);
+print("mutliplicacao", multiplicacao);
+print("divisao", divisao);
+print("resto", resto);
+print("potencia", potencia);

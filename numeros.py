1e5f746ee75642474086f2761a9553e04f1f5c06
Thiago Levino Nunes <Thiago.levino@outlook.com>
2023-06-22 23:28:32 -0300

    soma: números

--- numeros.py
+++ numeros.py
@@ -11,10 +11,21 @@ divisao           = num1 / num2;
 resto             = num1 % num2;
 potencia          = num1 ** num2;
 
-
+print("Operações matemáticas básicas: ");
 print("soma: ", soma);
 print("substracao: ", substracao);
 print("mutliplicacao", multiplicacao);
 print("divisao", divisao);
 print("resto", resto);
 print("potencia", potencia);
+
+#Arredodamento de número   
+numeroFloat =  3.14;
+numeroArrendodado = round(numeroFloat);
+print("Arrendodamento: " ,numeroArrendodado);
+
+#Funções matemáticas da biblioteca math 
+import math 
+
+num = 4.7;
+print  

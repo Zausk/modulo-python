2228cb912f5f6f760cc21cf84a987207897e91c7
Thiago Levino Nunes <Thiago.levino@outlook.com>
2023-06-30 23:10:08 -0300

    2

---
+++ Dicionario.py
@@ -0,0 +1,71 @@
+meuDicionario = {
+'nome'      : 'Thiago',
+'sobrenome' : 'Levino',
+'anos'      :  30
+}
+
+print(meuDicionario);
+
+frutaDicionario = {
+    'maçã'      : 3,
+    'banana'    : 6,
+    'uva'       : 8,
+};
+
+print("Significado encontrado no dicionario: ", frutaDicionario["maçã"]);
+print();
+frutaDicionario["maça"] = 5;
+frutaDicionario["laranja"] = 10;
+print("Significado encontrado no dicionario: ", frutaDicionario["maçã"]);
+print();
+print(frutaDicionario);
+
+print(frutaDicionario);
+print();
+animaisDicionario = {};
+animaisDicionario["cachorro"] = "Melissa";
+print(animaisDicionario);
+
+
+aluno = {
+    'nome'     : 'Thiago',
+    'idade'    : 35,
+    'hobbies'  : ['jogar', 'csgo',
+    'esporte' ]
+}
+
+print(aluno);
+
+frutaDicionario = {
+    'maçã'      : 3,
+    'banana'    : 6,
+    'uva'       : 8,
+    'laranja'   : 10
+};
+
+print();
+print("Quantidade de maçãs: ",
+frutaDicionario.get("maçã"));
+print("Quantidade de bananas: ", 
+frutaDicionario.get("banana"));
+
+print("Quantidade de morangos: ",
+frutaDicionario.get("morangos", "Não foi encontrado a definição de morango")); 
+
+elementoRemovido = frutaDicionario.pop("laranja");
+print(elementoRemovido);
+print("Dicionaário atualizado: ", 
+frutaDicionario);
+
+frutaDicionario = {
+    'maçã'      : 3,
+    'banana'    : 6,
+    'uva'       : 8,
+    'laranja'   : 10
+};
+
+print();
+print("Chaves encontrados no dicionário", frutaDicionario.keys());
+print("Valores encontrados no dicionário: ", frutaDicionario.values());
+print(frutaDicionario.items());
+
--- Listas_Tuplas_Dicionário/Atividade1/exercicio.py
+++ Listas_Tuplas_Dicionário/Atividade1/exercicio.py
@@ -36,25 +36,8 @@ print("Número formatado com 2 casas decimais {:.2f}".format(numeroFormatado))
 
 fraseNova = "Olá Python";
 print(fraseNova.upper());
-print(fraseNova.Lower());
 
-#Exemplos de programa que aborda o tipo de dado númerico em Python 
-
-#Operações matemáticas básicas
 num1 = 10;
-num2 = 5;
+num2 = 30;
+soma = num1 + num2;
 
-soma              = num1 + num2;
-substracao        = num1 - num2;
-multiplicacao     = num1 * num2;
-divisao           = num1 / num2;
-resto             = num1 % num2;
-potencia          = num1 ** num2;
-
-print("Operações matemáticas básicas: ");
-print("soma: ", soma);
-print("substracao: ", substracao);
-print("mutliplicacao", multiplicacao);
-print("divisao", divisao);
-print("resto", resto);
-print("potencia", potencia);

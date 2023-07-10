ed5618ab91e5b02a596ad275af54b5a29b84fc27
Thiago Levino Nunes <Thiago.levino@outlook.com>
2023-07-09 21:33:41 -0300

    Exercicio 4

---
+++ Exercicio 4/While.py
@@ -0,0 +1,41 @@
+contador = 10; 
+
+while contador >= 1:
+    print(contador);
+    contador -= 1;
+
+for numero in range (1, 11):
+    print(numero)
+    
+contador = 100;
+
+while contador >= 1:
+    print(contador);
+    contador -= 1;
+    
+for numero in range (1, 101):
+    print(numero)
+    
+contador = 20;
+
+while contador >= 1:
+    print(contador);
+    contador -=1
+    
+for numero in range (21):
+    print(numero)
+    
+num = 1
+
+while num <=1:
+    print (num, "Thiago")
+    num = num + 1 
+    
+minha_lista = []
+
+while True:
+    
+    numero = input("Digite um unico digito: ")
+    if numero == '-1':
+        break 
+    minha_lista.append(numero)
